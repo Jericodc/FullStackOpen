1be60a3d3511d684b618eb986e4609c716c7d7da
Jericodc <jericodc1@gmail.com>
2023-04-01 18:02:59 +0200

    Add % symbol to positive feedback percentage in statistics

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,40 +1,41 @@
 import { useState } from 'react'
 
-const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
-const StatisticsLine = ({text, value}) => (
+const StatisticLine = ({ text, value }) => (
   <tr>
     <td>{text}</td>
     <td>{value}</td>
   </tr>
 )
 
-const Statistics = ({good, neutral, bad}) => {
+const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
   const average = (good - bad) / all
-  const positive = (good / all) * 100
+  const positive = `${(good / all) * 100}%`
 
   if (all === 0) {
     return (
       <p>No feedback given</p>
     )
   }
+
   return (
     <table>
       <tbody>
-      <StatisticsLine text='Good' value={good} />
-      <StatisticsLine text='Neutral' value={neutral} />
-      <StatisticsLine text='Bad' value={bad} />
-      <StatisticsLine text='All' value={all} />
-      <StatisticsLine text='Average' value={average} />
-      <StatisticsLine text='Positive' value={positive} />
+        <StatisticLine text='Good' value={good} />
+        <StatisticLine text='Neutral' value={neutral} />
+        <StatisticLine text='Bad' value={bad} />
+        <StatisticLine text='All' value={all} />
+        <StatisticLine text='Average' value={average} />
+        <StatisticLine text='Positive' value={positive} />
       </tbody>
     </table>
   )
 }
 
 const App = () => {
-  
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -42,7 +43,7 @@ const App = () => {
   const handleGoodFeedback = () => setGood(good + 1)
   const handleNeutralFeedback = () => setNeutral(neutral + 1)
   const handleBadFeedback = () => setBad(bad + 1)
-  
+
   return (
     <div>
       <h1>Give Feedback</h1>
